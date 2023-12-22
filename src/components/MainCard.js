6e3d9d239748bea5e47d8bac309475c5681ad64a
mono0218 <mono@monokamo.com>
2023-12-22 17:22:15 +0900

    Add: create&delete

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -6,47 +6,84 @@ import {
 import {Button} from "@nextui-org/react";
 import {Card, CardBody} from "@nextui-org/react";
 import {useState} from "react";
+import { redirect,useRouter } from "next/navigation";
+import Link from "next/link";
 
-export default function MainCard(json){
-    const [data,setdata] = useState({})
 
+export default function MainCasrd(json){
     const onInputChange = (value) => {
         console.log(json.data[value -1])
         setdata(json.data[value -1])
     };
+
+    const [data,setdata] = useState(json.data[0])
+
     let count =0
 
     return(
-            <main className="p-20 h-full">
-                <div className="flex justify-between mb-10 items-center">
-                    <Autocomplete label="Select an Stage" className="max-w-xs" defaultItems={json.data[0].attributes.accessToken} defaultInputValue={json.data[0].attributes.accessToken} onSelectionChange={onInputChange}>
-                        {json.data.map((_data)=>{
-                            count +=1
-                            return(
-                                <AutocompleteItem key={count} textValue={_data.attributes.accessToken}>
-                                    <p>{_data.attributes.accessToken}</p>
-                                </AutocompleteItem>
-                            )
-                        })}
-                    </Autocomplete>
-
-                    <Button className="max-w-xs"> + New Stage</Button>
-                </div>
-
-
-                <Card className="h-full">
-                    <CardBody>
-                        <div className="flex justify-between">
-                            <div className="flex flex-col">
-
-                            </div>
-
-                            <div className="flex flex-col">
-                            </div>
-                        </div>
-                    </CardBody>
-                </Card>
-            </main>
-        )
-
+        <main className="p-20 h-full">
+            <div className="flex justify-between mb-10 items-center">
+                <Autocomplete label="Select an Stage" className="max-w-xs" defaultItems={json.data[0].attributes.accessToken} defaultInputValue={json.data[0].attributes.accessToken} onSelectionChange={onInputChange}>
+                    {json.data.map((_data)=>{
+                        count +=1
+                        return(
+                            <AutocompleteItem key={count} textValue={_data.attributes.accessToken}>
+                                <p>{_data.attributes.accessToken}</p>
+                            </AutocompleteItem>
+                        )
+                    })}
+                </Autocomplete>
+                <Button className="max-w-xs" onClick={()=>{createStage().then((data) => json.data.push(data))}}> + New Stage</Button>
+            </div>
+            <DataCard {...data}/>
+        </main>
+    )
 }
+
+function DataCard(data){
+    if(data !== undefined){
+        return (
+            <Card className="h-full">
+                <CardBody>
+                    <div className="flex justify-between">
+                        <div className="flex flex-col">
+                            {data.id}
+
+                        </div>
+                        <div className="flex flex-col">
+                            <Button className="max-w-xs" color="danger" onClick={()=>{deleteStage(data.id).then()}}> Remove</Button>
+                        </div>
+                    </div>
+                </CardBody>
+            </Card>
+        )
+    }else{
+        return (
+            <Card className="h-full">
+                <CardBody>
+                    <div className="flex justify-between">
+                        <div className="flex flex-col">
+                            データがありません。ステージを選択するか、作成してください
+                        </div>
+                        <div className="flex flex-col">
+                        </div>
+                    </div>
+                </CardBody>
+            </Card>
+        )
+    }
+}
+
+async function createStage(){
+    const data = await fetch("/api/createStage",{
+        method: "POST"
+    })
+    return data
+}
+
+async function deleteStage(id){
+    const data= await fetch(`/api/deleteStage/${id}`,{
+        method: "DELETE"
+    })
+}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const {nextui} = require("@nextui-org/react");
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -14,5 +16,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
