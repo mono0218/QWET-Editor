2b6a2293047d4eb1b3f8bd4a365e30a82e41f455
mono0218 <mono@monokamo.com>
2023-12-23 19:52:28 +0900

    Add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import SiteNav from "@/components/nav"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={"dark"}>
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={"dark w-full h-full"}>
+      <body className={inter.className}>
+        <SiteNav/>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/components/nav.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+export default function App() {
+    return (
+        <Navbar className={"w-screen"}>
+            <NavbarBrand>
+                <p className="font-bold text-inherit">WEB-LIVE</p>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Features
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="#" aria-current="page">
+                        Customers
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Integrations
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="/login">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="/signup" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
+}
