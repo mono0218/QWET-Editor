026fe4a8a0fb5a74fde6a9a52d7a59f9a17f5d6e
mono0218 <mono@monokamo.com>
2023-12-23 14:35:57 +0900

    Change: TypeScript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-const {nextui} = require("@nextui-org/react");
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -16,6 +15,6 @@ module.exports = {
       },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 }
+export default config
