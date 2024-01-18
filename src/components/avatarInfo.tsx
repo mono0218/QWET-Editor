1fbce72a389f5de7fe3f57890d855891d6f72b25
mono0218 <mono@monokamo.com>
2024-01-18 09:27:32 +0900

    WIP: UserPage&TopPage

--- src/components/avatarInfo.tsx
+++ src/components/avatarInfo.tsx
@@ -6,7 +6,7 @@ export default function AvatarInfo(data:CharacterModel){
         <>
             <div>
                 <div className="flex felx-col max-w-[170px]">
-                    <Image className={"p-0 m-0"} src={data.portrait_image.w300.url}></Image>
+                    <Image  className={"p-0 m-0"} src={data.portrait_image.w300.url}></Image>
                 </div>
 
                 <p className={"mt-3"}>{data.name}</p>
