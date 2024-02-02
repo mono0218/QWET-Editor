4a98648498d9b84831f2dcb788748840ea47c76f
mono0218 <81796635+mono0218@users.noreply.github.com>
2024-02-02 16:28:52 +0900

    Feature/UI fix (#35)
    * Fix: UI
    
    * Fix: LintError

--- src/app/(livesystem)/room/page.tsx
+++ src/app/(livesystem)/room/page.tsx
@@ -16,6 +16,9 @@ export default function Page() {
   const [Data, setData] = useState([]);
 
   const [isSend, setIsSend] = useState(false);
+  const [motion, setMotion] = useState("");
+  const [stage, setStage] = useState("");
+
   const router = useRouter();
 
   useEffect(() => {
@@ -24,12 +27,16 @@ export default function Page() {
       const _avatar = await avatarData.json();
       const avatar = _avatar.data;
       setData(avatar);
+
       console.log(localStorage.getItem("motion"))
       const motion = localStorage.getItem("motion")
       const stage = localStorage.getItem("stage")
+
       if(motion == null || stage == null) {
         alert("モーションとステージを選択してからアクセスしてください")
       }
+      setMotion(motion)
+      setStage(stage)
     })();
   }, []);
 
@@ -52,9 +59,6 @@ export default function Page() {
 
     await router.push(`/room/${result.uuid}`);
   };
-
-  const motion = localStorage.getItem("motion")
-  const stage = localStorage.getItem("stage")
   return (
     <div className="ml-72 mr-72 mt-10">
       {Data ? (
@@ -66,7 +70,7 @@ export default function Page() {
             <Input
               isDisabled
               type="string"
-              defaultValue= {stage}
+              value= {stage}
               label="ステージUUID"
               name="stageUUID"
             />
@@ -74,7 +78,7 @@ export default function Page() {
             <Input
               isDisabled
               type="string"
-              defaultValue= {motion}
+              value= {motion}
               label="モーションUUID"
               name="motionUUID"
             />
@@ -83,7 +87,7 @@ export default function Page() {
 
             <h1 className="text-2xl font-bold text-center">アバター選択</h1>
             <ScrollShadow className="w-full h-[400px]">
-              <RadioGroup name="avatarUrl">
+              <RadioGroup name="avatarUrl" >
                 <div className="grid gap-x-8 gap-y-4 grid-cols-6">
                   {Data.map((_data: CharacterModel) => (
                     <div key={_data.id}>
