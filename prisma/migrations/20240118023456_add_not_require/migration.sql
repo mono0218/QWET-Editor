1f18eeca30484eab85d088a76525192ca7166eb4
mono0218 <mono@monokamo.com>
2024-03-03 21:35:35 +0900

    Add: ReactVite Setup

--- prisma/migrations/20240118023456_add_not_require/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE `Motion` MODIFY `content` VARCHAR(191) NULL,
-    MODIFY `license` VARCHAR(191) NULL;
-
--- AlterTable
-ALTER TABLE `Stage` MODIFY `content` VARCHAR(191) NULL,
-    MODIFY `license` VARCHAR(191) NULL;
--- prisma/migrations/20240124131444_fix_live_model/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `movieUrl` to the `Live` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Live` ADD COLUMN `movieUrl` VARCHAR(191) NOT NULL,
-    MODIFY `avaterId` VARCHAR(191) NOT NULL;
