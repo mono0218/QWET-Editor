a3cab2a8e519f5c9d21ff0e98074c0a15545acfd
mono0218 <81796635+mono0218@users.noreply.github.com>
2024-03-26 21:33:34 +0900

    Fix: AvatarSetting (#59)
    * Fix: AvatarSetting
    
    * Apply Prettier Change
    
    ---------
    
    Co-authored-by: mono0218 <mono0218@users.noreply.github.com>

--- src/lib/object/importObject.ts
+++ src/lib/object/importObject.ts
@@ -1,12 +1,7 @@
-import {
-    AbstractMesh,
-    Node,
-    Scene,
-    SceneLoader,
-    TransformNode,
-} from '@babylonjs/core'
+import { Scene, SceneLoader, TransformNode } from '@babylonjs/core'
 import '@babylonjs/loaders/glTF'
 import { tfNodeManager } from '../manager/tfNodeManager'
+import { getParent } from './getParent'
 
 export function importObject({
     scene,
@@ -25,24 +20,4 @@ export function importObject({
         rootMesh.parent = rootNode
         manager.addNode(rootNode)
     })
-}
-
-function getParent(_3DObject: AbstractMesh) {
-    let child = _3DObject
-    child.parent
-    let parent
-    do {
-        if (child.parent === null) {
-            parent = _3DObject
-            break
-        } else {
-            parent = child.parent
-            if (parent instanceof Node) {
-                parent = child as AbstractMesh
-                break
-            }
-            child = parent
-        }
-    } while (child.parent !== null)
-    return parent
 }
